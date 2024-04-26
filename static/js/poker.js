134ad2a2bfae5a219bb587f8558f5bed0d2aae6c
Madhur Ahuja <ahuja.madhur@gmail.com>
2024-04-26 11:11:55 +0530

    feat: changes for websocket

--- static/js/poker.js
+++ static/js/poker.js
@@ -244,242 +244,359 @@ testhand = {
   }
 };
 
-function catchUp(){
-    var round = testhand['PokerHand']['Rounds'].length - 1;
+function catchUp() {
+  // var round = testhand['PokerHand']['Rounds'].length - 1;
 
-    setBoard();
-    setHeroCards();
-    setOpponentCards();
-    setPlayerNames();
-    setPlayerStacks();
-    setPots();
+  setBoard();
+  setHeroCards();
+  setOpponentCards();
+  setPlayerNames();
+  setPlayerStacks();
+  setPots();
 
-   // curActions = testhand['PokerHand']['Rounds'][round]['Actions'];
-    //curBets = [0,0,0,0,0,0,0];
-    addCommittedAmounts(curActions, curBets, testhand);
-    for (var i = 0; i < curBets.length; i++)
-        setBet(curBets[i], i+1);
+  // curActions = testhand['PokerHand']['Rounds'][round]['Actions'];
+  //curBets = [0,0,0,0,0,0,0];
+  addCommittedAmounts(curActions, curBets, testhand);
+  for (var i = 0; i < curBets.length; i++)
+    setBet(curBets[i], i + 1);
 
-    moveButton(targetSeat + 1);
-    targetSeat = (targetSeat + 1) % 6;
+  moveButton(targetSeat + 1);
+  targetSeat = (targetSeat + 1) % 6;
 }
 
 function updateGameState() {
-    var sidebar = $('#poker_table');
-    var requestData = {
-        url: sidebar.data('uid')
-    };
-    catchUp();
+  var sidebar = $('#poker_table');
+  var requestData = {
+    url: sidebar.data('uid')
+  };
+  catchUp();
 }
 
 function setTimer(val) {
-    var timer = $('#game-timer');
-    timer.removeClass();
-    if (val < 10)
-        timer.addClass("timer-warning");
-    else
-        timer.addClass("timer-normal");
-    
-    if (val < 0)
-        val = 0;
+  var timer = $('#game-timer');
+  timer.removeClass();
+  if (val < 10)
+    timer.addClass("timer-warning");
+  else
+    timer.addClass("timer-normal");
 
-    timer.text(val + " seconds remaining.");
-    timer.show();
+  if (val < 0)
+    val = 0;
+
+  timer.text(val + " seconds remaining.");
+  timer.show();
 }
 function hideTimer() {
-    $('#game-timer').hide();
+  $('#game-timer').hide();
 }
 
-function setPlayerNames(playerids){
-    players = playerids
-    for(var i = 0; i < players.length; i++)
-        setPlayerName(playerids[i],  playerids[i]);
+function setPlayerNames(playerids) {
+  players = playerids
+  for (var i = 0; i < players.length; i++)
+    setPlayerName(playerids[i], playerids[i]);
 }
 
-function setPlayerName(name, seat){
-    var namediv = $('#seat' + seat).children('.name-chips').children('.player-name');
-    namediv.text(name);
+function setPlayerName(name, seat) {
+  var namediv = $('#seat' + seat).children('.name-chips').children('.player-name');
+  namediv.text(name);
 }
 
-function setPlayerStacks(hand){
-    committed = [0,0,0,0,0,0,0];
-    blinds = hand['PokerHand']['Blinds'];
-    addCommittedAmounts(blinds, committed, hand);
+function setPlayerStacks(hand) {
+  committed = [0, 0, 0, 0, 0, 0, 0];
+  blinds = hand['PokerHand']['Blinds'];
+  addCommittedAmounts(blinds, committed, hand);
 
-    // var rounds = hand['PokerHand']['Rounds'];
-    // for (var i = 0; i < rounds.length; i++)
-    //     addCommittedAmounts(rounds[i], committed, hand);
+  // var rounds = hand['PokerHand']['Rounds'];
+  // for (var i = 0; i < rounds.length; i++)
+  //     addCommittedAmounts(rounds[i], committed, hand);
 
-    // players = hand['PokerHand']['Players'];
-    // for(var i = 0; i < players.length; i++){
-    //     stack = parseFloat(players[i]['Stack']);
-    //     seat = parseInt(players[i]['Seat']);
-    //     setPlayerStack(stack - committed[seat], seat);
-    // }
+  // players = hand['PokerHand']['Players'];
+  // for(var i = 0; i < players.length; i++){
+  //     stack = parseFloat(players[i]['Stack']);
+  //     seat = parseInt(players[i]['Seat']);
+  //     setPlayerStack(stack - committed[seat], seat);
+  // }
 }
 
-function setPlayerStack(amount, seat){
-    chipsdiv = $('#seat' + seat).children('.name-chips').children('.chips');
-    if(amount == null)
-        chipsdiv.text("");
-    else
-        chipsdiv.text("$" + amount);
+function setPlayerStack(amount, seat) {
+  chipsdiv = $('#seat' + seat).children('.name-chips').children('.chips');
+  if (amount == null)
+    chipsdiv.text("");
+  else
+    chipsdiv.text("$" + amount);
 }
 
-function addCommittedAmounts(actions, committed, hand){
-    for(var aidx = 0; aidx < actions.length; aidx++){
-        var amt = actions[aidx]['Amount'];
-        if(amt == null)
-            continue;
-        amt = parseFloat(amt);
-        seat = findSeatNumber(actions[aidx]['Player'], hand);
-        committed[seat] += amt;
-    }
+function addCommittedAmounts(actions, committed, hand) {
+  for (var aidx = 0; aidx < actions.length; aidx++) {
+    var amt = actions[aidx]['Amount'];
+    if (amt == null)
+      continue;
+    amt = parseFloat(amt);
+    seat = findSeatNumber(actions[aidx]['Player'], hand);
+    committed[seat] += amt;
+  }
 }
 
 function getCardSrc(rank, suit) {
-    rank = rank.toLowerCase();
-    if(rank == "two")
-        rank = 2;
-    else if (rank == "three")
-        rank = 3;
-    else if (rank == "four")
-        rank = 4;
-    else if (rank == "five")
-        rank = 5;
-    else if (rank == "six")
-        rank = 6;
-    else if (rank == "seven")
-        rank = 7;
-    else if (rank == "eight")
-        rank = 8;
-    else if (rank == "nine")
-        rank = 9;
-    else if (rank == "ten")
-        rank = 10;
-    return "url('static/images/" + rank + "_of_" + suit.toLowerCase() + ".png')";
+  rank = rank.toLowerCase();
+  if (rank == "two")
+    rank = 2;
+  else if (rank == "three")
+    rank = 3;
+  else if (rank == "four")
+    rank = 4;
+  else if (rank == "five")
+    rank = 5;
+  else if (rank == "six")
+    rank = 6;
+  else if (rank == "seven")
+    rank = 7;
+  else if (rank == "eight")
+    rank = 8;
+  else if (rank == "nine")
+    rank = 9;
+  else if (rank == "ten")
+    rank = 10;
+  return "url('static/images/" + rank + "_of_" + suit.toLowerCase() + ".png')";
 }
 
 function setHeroCards(hand) {
-    var hero = hand['PokerHand']['Hero'];
-    var seat = findSeatNumber(hero, hand);
-    var hc = hand['PokerHand']['HoleCards'];
-    setHoleCards(hc, seat);
+  var hero = hand['PokerHand']['Hero'];
+  var seat = findSeatNumber(hero, hand);
+  var hc = hand['PokerHand']['HoleCards'];
+  setHoleCards(hc, seat);
 }
 
-function setOpponentCards(hand){
-    results = hand['PokerHand']['Results'];
-    if (results == null)
-        return;
-    for (var i = 0; i < results.length; i++){
-        hc = results[i]['HoleCards'];
-        if (hc == null)
-            continue;
-        setHoleCards(hc, findSeatNumber(results[i]['Player'], hand));
-    }
+function setOpponentCards(hand) {
+  results = hand['PokerHand']['Results'];
+  if (results == null)
+    return;
+  for (var i = 0; i < results.length; i++) {
+    hc = results[i]['HoleCards'];
+    if (hc == null)
+      continue;
+    setHoleCards(hc, findSeatNumber(results[i]['Player'], hand));
+  }
 }
 
-function findSeatNumber(player, hand){
-    seats = hand['PokerHand']['Players']
-    for (var i = 0; i < seats.length; i++){
-        if (seats[i]['Name'] == player)
-            return parseInt(seats[i]['Seat'])
-    }
-    return -1;
+function findSeatNumber(player, hand) {
+  seats = hand['PokerHand']['Players']
+  for (var i = 0; i < seats.length; i++) {
+    if (seats[i]['Name'] == player)
+      return parseInt(seats[i]['Seat'])
+  }
+  return -1;
 }
 
-function setHoleCards(hc, seat){    
-    var holecards = $('#seat' + seat).children('.holecards');
-    var card1 = holecards.children('.holecard1');
-    var card2 = holecards.children('.holecard2');
+function setHoleCards(hc, seat) {
+  var holecards = $('#seat' + seat).children('.holecards');
+  var card1 = holecards.children('.holecard1');
+  var card2 = holecards.children('.holecard2');
 
-    setCard(card1, hc[0]);
-    setCard(card2, hc[1]);
+  setCard(card1, hc[0]);
+  setCard(card2, hc[1]);
 }
 
-function setBoard(hand){
-    // var round = testhand['PokerHand']['Rounds'].length - 1;
-    // if (round >= 1){
-    //     setFlop(testhand['PokerHand']['Rounds'][1]['CommunityCards']);
-    //     if(round >= 2){
-    //         setTurn(testhand['PokerHand']['Rounds'][2]['CommunityCards']);
-    //         if(round >= 3){
-    //             setRiver(testhand['PokerHand']['Rounds'][3]['CommunityCards']);
-    //         }
-    //     }         
-    // }
+function setBoard(hand) {
+  // var round = testhand['PokerHand']['Rounds'].length - 1;
+  // if (round >= 1){
+  //     setFlop(testhand['PokerHand']['Rounds'][1]['CommunityCards']);
+  //     if(round >= 2){
+  //         setTurn(testhand['PokerHand']['Rounds'][2]['CommunityCards']);
+  //         if(round >= 3){
+  //             setRiver(testhand['PokerHand']['Rounds'][3]['CommunityCards']);
+  //         }
+  //     }         
+  // }
 
 
 }
 
 function setFlop(flop) {
-    setCard($('#flop1'), flop[0]);
-    setCard($('#flop2'), flop[1]);
-    setCard($('#flop3'), flop[2]);
+  setCard($('#flop1'), flop[0]);
+  setCard($('#flop2'), flop[1]);
+  setCard($('#flop3'), flop[2]);
 }
 
 function setTurn(turn) {
-    setCard($('#turn'), turn);
+  setCard($('#turn'), turn);
 }
 
 function setRiver(river) {
-    setCard($('#river'), river);
+  setCard($('#river'), river);
 }
 
-function setCard(div, card){
-    div.css('background-image', getCardSrc(card['Rank'], card['Suit']))
+function setCard(div, card) {
+  div.css('background-image', getCardSrc(card['Rank'], card['Suit']))
 }
 
-function setBet(bet, seat){
-    var betdiv = $('#seat' + seat).children('.bet');
-    if (bet == null || bet == 0)
-        betdiv.text('');
-    else
-        betdiv.text('$' + bet);
+function setBet(bet, seat) {
+  var betdiv = $('#seat' + seat).children('.bet');
+  if (bet == null || bet == 0)
+    betdiv.text('');
+  else
+    betdiv.text('$' + bet);
 }
 
-function setPots(curPot, totalPot){
-    
+function setPots(curPot, totalPot) {
 
-    setCurrentPot(curPot);
-    setTotalPot(totalPot);
+
+  setCurrentPot(curPot);
+  setTotalPot(totalPot);
 }
 
-function setTotalPot(amount){
-    $('#total-pot').text('Total pot: $' + amount.toFixed(2));
+function setTotalPot(amount) {
+  $('#total-pot').text('Total pot: $' + amount.toFixed(2));
 }
 
-function setCurrentPot(amount){
-    if(amount == 0)
-        $('#current-pot').text("");
-    else
-        $('#current-pot').text('$' + amount.toFixed(2));
+function setCurrentPot(amount) {
+  if (amount == 0)
+    $('#current-pot').text("");
+  else
+    $('#current-pot').text('$' + amount.toFixed(2));
 }
 
-function getChipSrc(chipValue){
-    
+function getChipSrc(chipValue) {
+
 }
 
-function foldClicked(){
-    
+function foldClicked() {
+
 }
 
-function callClicked(){
-    
+function callClicked() {
+
 }
 
-function raiseClicked(){
-    
+function raiseClicked() {
+
 }
 
-function resetTable(){
-    
+function resetTable() {
+
 }
 
-function moveButton(seat){
-    var button = $('#button');
-    button.removeClass();
-    button.addClass('seat' + seat + '-button');
+function moveButton(seat) {
+  var button = $('#button');
+  button.removeClass();
+  button.addClass('seat' + seat + '-button');
 }
 
-updateGameState();
+setupwebsocket();
+
+
+// Global vars
+var socket = null;
+var user_id = null;
+
+// Global vars end
+
+function setupwebsocket() {
+
+
+  const queryString = window.location.search;
+
+  // Create a URLSearchParams object from the query string
+  const params = new URLSearchParams(queryString);
+
+  // Get the value of a specific parameter by its name
+  user_id = params.get('user_id');
+
+  if (!user_id) {
+    console.log("user_id not found, returning")
+    return;
+  }
+
+  socket = new WebSocket('ws://localhost:8080');
+
+
+
+  // Interval for sending heartbeats (in milliseconds)
+  const heartbeatInterval = 5000; // 5 seconds
+
+  function sendHeartbeat() {
+    if (socket.readyState === WebSocket.OPEN) {
+      socket.send(`{"type": "heartbeatreq", "src": ${user_id}}\n`);
+    }
+  }
+
+  function sendSetup() {
+    if (socket.readyState === WebSocket.OPEN) {
+      socket.send(`{"type": "setup", "sid": "test", "src": ${user_id}}\n`);
+    }
+  }
+
+
+  socket.addEventListener('open', function (event) {
+    console.log('WebSocket connection established.');
+    // Send the first heartbeat immediately upon connection
+
+    sendSetup();
+    sendHeartbeat();
+
+    // Schedule sending heartbeats at regular intervals
+    setInterval(sendHeartbeat, heartbeatInterval);
+  });
+
+  socket.addEventListener('message', function (event) {
+    const data = JSON.parse(event.data);
+    console.log("Incoming message", data);
+    if (data.type === 'INITIALIZE_HAND') {
+      handleHandInitMessage(data);
+    }
+
+  });
+
+  socket.onclose = () => {
+    console.log('WebSocket connection closed.');
+  };
+
+  // Event handler for WebSocket error event
+  socket.onerror = (error) => {
+    console.error('WebSocket error:', error);
+  };
+
+  updateGameState();
+
+}
+
+function handleHandInitMessage(handInitMessage) {
+  $('.seat').hide();
+  $('.seat').hide();
+  handInitMessage.users.forEach(uid=>{
+      $('#seat' + uid).show();
+  });
+  handInitMessage.users.forEach(uid=>{
+    $('#seat' + uid  + ' chips').text(handInitMessage.initialAmounts[0])
+  });
+  handInitMessage.users.forEach(uid=>{
+    $('#seat' + uid  + ' .bet').text("0")
+  });
+  var sbUserId, bbUserId, dealerUserId;
+  sbUserId = handInitMessage.users[handInitMessage.smallBlindPosition];
+  bbUserId = handInitMessage.users[handInitMessage.bigBlindPosition];
+  dealerUserId = handInitMessage.users[handInitMessage.dealerPosition];
+
+  moveButton(dealerUserId)
+  let  sbplayerName = $('#seat' + sbUserId + ' .player-name').text();
+  $('#seat' + sbUserId + ' .player-name').text(sbplayerName + " SB");
+
+  let  bbplayerName = $('#seat' + bbUserId + ' .player-name').text();
+  $('#seat' + bbUserId + ' .player-name').text(bbplayerName + " BB");
+
+  $('#board').hide();
+  $('#total-pot').text("0")
+
+}
+
+
+function start_game() {
+
+  if (socket  == null) {
+    console.error("Socket not connected");
+    return;
+  }
+
+  socket.send(`{"type": "start_game", "sid": "test", "src": ${user_id}}\n`);
+
+}
